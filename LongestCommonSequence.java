494d19963b3dcaf85cf1c3e13f0fce1b0918bcb3
pritirajdev11 <rajdevpriti2000@gmail.com>
2023-03-15 12:06:57 +0530

    added existing file

---
+++ LongestCommonSequence.java
@@ -0,0 +1,24 @@
+package firstpackage;
+public class LongestCommonSequence {
+	    public static String findLongestCommonSequence(String s1, String s2) {
+	        String result = "";
+	        int[][] matrix = new int[s1.length()+1][s2.length()+1];
+	        for (int i = 0; i < s1.length(); i++) {
+	            for (int j = 0; j < s2.length(); j++) {
+	                if (s1.charAt(i) == s2.charAt(j)) {
+	                    matrix[i+1][j+1] = matrix[i][j] + 1;
+	                    if (matrix[i+1][j+1] > result.length()) {
+	                        result = s1.substring(i-matrix[i+1][j+1]+1, i+1);
+	                    }
+	                }
+	            }
+	        }
+	        return result;
+	    }
+	    
+	    public static void main(String[] args) {
+	        String s1 = "abcdefghik";
+	        String s2 = "aecfghikcs";
+	        System.out.println("Longest Common subString is:"+findLongestCommonSequence(s1, s2)); // "cfghik"
+	    }
+	}
